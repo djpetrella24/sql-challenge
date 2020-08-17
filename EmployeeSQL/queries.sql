75910ac7256b98091f7ae382bfbf0ffe61fcd0fb
Daniel Petrella <djpetrella24@gmail.com>
2020-08-17 17:31:40 -0400

    All questions complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -12,14 +12,38 @@ FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dm.emp_no, dm.dept_no, d.dept_name, e.last_name, e.first_name
+FROM dept_manager AS dm
+LEFT JOIN departments AS d ON dm.dept_no = d.dept_no
+INNER JOIN employees AS e ON e.emp_no = dm.emp_no
 
+SELECT * from titles
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+INNER JOIN departments as d ON d.dept_no = de.dept_no
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
+FROM employees as e
+INNER JOIN dept_emp as de ON de.emp_no = e.emp_no
+INNER JOIN departments as d ON d.dept_no = de.dept_no
+WHERE dept_name = 'Sales'
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
+FROM employees as e
+INNER JOIN dept_emp as de ON de.emp_no = e.emp_no
+INNER JOIN departments as d ON d.dept_no = de.dept_no
+WHERE dept_name IN ('Sales', 'Development')
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC
